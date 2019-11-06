319914f11eed4674ad5948ca4f90a9eb97252aad
NituShrestha <shresthanitu984@gmail.com>
2019-11-06 14:41:04 +0545

    Added - Styling option for Counter 1 widget

--- controls/content-widget-counter-1.php
+++ controls/content-widget-counter-1.php
@@ -23,6 +23,10 @@
 
 use Elementor\SPT_COUNTER_1;
 use Elementor\Controls_Manager;
+use Elementor\Scheme_Color;
+use ELementor\Group_Control_Typography;
+use ELementor\Group_Control_Border;
+use Elementor\Scheme_Typography;
 
 // Exit if it is accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
@@ -74,6 +78,165 @@ $instance->add_control(
 		'label'   => esc_html__( 'Title', 'spacious-toolkit' ),
 		'type'    => Controls_Manager::TEXT,
 		'default' => esc_html__( 'Projects', 'spacious-toolkit' ),
+	)
+);
+
+$instance->end_controls_section();
+
+// Style for counter 1 icon.
+$instance->start_controls_section(
+	'section_counter_1_style',
+	array(
+		'label' => __( 'Icon', 'spacious-toolkit' ),
+		'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+	)
+);
+
+$instance->add_control(
+	'counter_1_icon_color',
+	array(
+		'label'     => esc_html__( 'Color', 'spacious-toolkit' ),
+		'type'      => Controls_Manager::COLOR,
+		'default'   => '#78b865',
+		'scheme'    => array(
+			'type'  => Scheme_Color::get_type(),
+			'value' => Scheme_Color::COLOR_1,
+		),
+		'selectors' => array(
+			'{{WRAPPER}} .counter .counter__icon' => 'color: {{VALUE}}',
+
+		),
+	)
+);
+
+$instance->add_control(
+	'counter_1_icon_size',
+	array(
+		'label' => __( 'Size', 'spacious-toolkit-toolkit' ),
+		'type' => Controls_Manager::SLIDER,
+		'size_units' => array( 'px', '%' ),
+		'range' => array(
+			'px' => array(
+				'min' => 0,
+				'max' => 100,
+				'step' => 1,
+			),
+			'%' => array(
+				'min' => 0,
+				'max' => 100,
+			),
+		),
+		'default' => array(
+			'unit' => 'px',
+			'size' => 40,
+		),
+		'selectors' => array(
+			'{{WRAPPER}} .counter .counter__icon' => 'font-size: {{SIZE}}{{UNIT}};',
+		),
+	)
+);
+
+$instance->end_controls_section();
+
+// For number.
+$instance->start_controls_section(
+	'section_counter_1_number_style',
+	array(
+		'label' => __( 'Number', 'spacious-toolkit' ),
+		'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+	)
+);
+
+$instance->add_control(
+	'counter_1_number_color',
+	array(
+		'label'     => esc_html__( 'Color', 'spacious-toolkit' ),
+		'type'      => Controls_Manager::COLOR,
+		'default'   => '#1e1e1f',
+		'scheme'    => array(
+			'type'  => Scheme_Color::get_type(),
+			'value' => Scheme_Color::COLOR_1,
+		),
+		'selectors' => array(
+			'{{WRAPPER}} .counter .counter__number' => 'color: {{VALUE}}',
+
+		),
+	)
+);
+
+$instance->add_group_control(
+	Group_Control_Typography::get_type(),
+	array(
+		'name'     => 'counter_1_number_typography',
+		'label'    => __( 'Typography', 'spacious-toolkit' ),
+		'scheme'   => Scheme_Typography::TYPOGRAPHY_1,
+		'selector' => '{{WRAPPER}} .counter .counter__number',
+	)
+);
+
+$instance->end_controls_section();
+
+// For divider.
+$instance->start_controls_section(
+	'section_counter_1_divider_style',
+	array(
+		'label' => __( 'Divider', 'spacious-toolkit' ),
+		'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+	)
+);
+
+$instance->add_control(
+	'counter_1_divider_color',
+	array(
+		'label'     => esc_html__( 'Color', 'spacious-toolkit' ),
+		'type'      => Controls_Manager::COLOR,
+		'default'   => '#78b865',
+		'scheme'    => array(
+			'type'  => Scheme_Color::get_type(),
+			'value' => Scheme_Color::COLOR_1,
+		),
+		'selectors' => array(
+			'{{WRAPPER}} .counter .counter__number:before' => 'background: {{VALUE}}',
+
+		),
+	)
+);
+
+$instance->end_controls_section();
+
+// For titile.
+$instance->start_controls_section(
+	'section_counter_1_title_style',
+	array(
+		'label' => __( 'Title', 'spacious-toolkit' ),
+		'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+	)
+);
+
+$instance->add_control(
+	'counter_1_title_color',
+	array(
+		'label'     => esc_html__( 'Color', 'spacious-toolkit' ),
+		'type'      => Controls_Manager::COLOR,
+		'default'   => '#807f83',
+		'scheme'    => array(
+			'type'  => Scheme_Color::get_type(),
+			'value' => Scheme_Color::COLOR_1,
+		),
+		'selectors' => array(
+			'{{WRAPPER}} .counter .counter__title' => 'color: {{VALUE}}',
+
+		),
+	)
+);
+
+$instance->add_group_control(
+	Group_Control_Typography::get_type(),
+	array(
+		'name'     => 'counter_1_title_typography',
+		'label'    => __( 'Typography', 'spacious-toolkit' ),
+		'scheme'   => Scheme_Typography::TYPOGRAPHY_1,
+		'selector' => '{{WRAPPER}} .counter .counter__title',
 	)
 );
 
