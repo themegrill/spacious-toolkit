57aba8d317cdfc887fc6e0f0d5560f44046252c5
NituShrestha <shresthanitu984@gmail.com>
2019-11-07 14:43:51 +0545

    Add - Styling option for Team 2 widget

--- controls/content-widget-team-2.php
+++ controls/content-widget-team-2.php
@@ -24,6 +24,10 @@
 use Elementor\SPT_TEAM_2;
 use Elementor\Controls_Manager;
 use Elementor\Utils;
+use ELementor\Group_Control_Typography;
+use Elementor\Scheme_Typography;
+use ELementor\Group_Control_Border;
+use Elementor\Scheme_Color;
 
 // Exit if it is accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
@@ -77,6 +81,144 @@ $instance->add_control(
 		'label'   => esc_html__( 'Description', 'spacious-toolkit' ),
 		'type'    => Controls_Manager::TEXTAREA,
 		'default' => esc_html__( 'Description of the member goes here.', 'spacious-toolkit' ),
+	)
+);
+
+$instance->end_controls_section();
+
+// Team two Style.
+$instance->start_controls_section(
+	'section_team2_box',
+	array(
+		'label' => __( 'Team Box', 'spacious' ),
+		'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+	)
+);
+
+$instance->add_control(
+	'team2_background_color',
+	array(
+		'label'     => esc_html__( 'Background', 'spacious' ),
+		'type'      => Controls_Manager::COLOR,
+		'default'   => 'rgba(120,184,101,0.5)',
+		'scheme'    => array(
+			'type'  => Scheme_Color::get_type(),
+			'value' => Scheme_Color::COLOR_1,
+		),
+		'selectors' => array(
+			'{{WRAPPER}} .team-member__info' => 'background-color: {{VALUE}}',
+		),
+	)
+);
+
+$instance->end_controls_section();
+
+// Name Style
+$instance->start_controls_section(
+	'section_team2_name',
+	array(
+		'label' => __( 'Name', 'spacious' ),
+		'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+	)
+);
+
+$instance->add_control(
+	'team_two_name',
+	array(
+		'label'     => esc_html__( 'Color', 'spacious' ),
+		'type'      => Controls_Manager::COLOR,
+		'default'   => '#ffffff',
+		'scheme'    => array(
+			'type'  => Scheme_Color::get_type(),
+			'value' => Scheme_Color::COLOR_1,
+		),
+		'selectors' => array(
+			'{{WRAPPER}} .team-member__title' => 'color: {{VALUE}}',
+		),
+	)
+);
+
+$instance->add_group_control(
+	Group_Control_Typography::get_type(),
+	array(
+		'name'     => 'team_two_name_typography',
+		'label'    => __( 'Typography', 'spacious' ),
+		'scheme'   => Scheme_Typography::TYPOGRAPHY_1,
+		'selector' => '{{WRAPPER}} .team-member__title',
+	)
+);
+
+$instance->end_controls_section();
+
+// Designation Style
+$instance->start_controls_section(
+	'section_team2_designation',
+	array(
+		'label' => __( 'Designation', 'spacious' ),
+		'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+	)
+);
+
+$instance->add_control(
+	'team_two_designation',
+	array(
+		'label'     => esc_html__( 'Color', 'spacious' ),
+		'type'      => Controls_Manager::COLOR,
+		'default'   => '#ffffff',
+		'scheme'    => array(
+			'type'  => Scheme_Color::get_type(),
+			'value' => Scheme_Color::COLOR_1,
+		),
+		'selectors' => array(
+			'{{WRAPPER}} .team-member__position' => 'color: {{VALUE}}',
+		),
+	)
+);
+
+$instance->add_group_control(
+	Group_Control_Typography::get_type(),
+	array(
+		'name'     => 'team_two_designation_typography',
+		'label'    => __( 'Typography', 'spacious' ),
+		'scheme'   => Scheme_Typography::TYPOGRAPHY_1,
+		'selector' => '{{WRAPPER}} .team-member__position',
+	)
+);
+
+$instance->end_controls_section();
+
+// Description Style.
+$instance->start_controls_section(
+	'section_team2_description',
+	array(
+		'label' => __( 'Description', 'spacious' ),
+		'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+	)
+);
+
+$instance->add_control(
+	'team_two_description',
+	array(
+		'label'     => esc_html__( 'Color', 'spacious' ),
+		'type'      => Controls_Manager::COLOR,
+		'default'   => '#ffffff',
+		'scheme'    => array(
+			'type'  => Scheme_Color::get_type(),
+			'value' => Scheme_Color::COLOR_1,
+		),
+		'selectors' => array(
+			'{{WRAPPER}} .team-member__description' => 'color: {{VALUE}}',
+		),
+	)
+);
+
+$instance->add_group_control(
+	Group_Control_Typography::get_type(),
+	array(
+		'name'     => 'team_two_description_typography',
+		'label'    => __( 'Typography', 'spacious' ),
+		'scheme'   => Scheme_Typography::TYPOGRAPHY_1,
+		'selector' => '{{WRAPPER}} .team-member__description',
 	)
 );
 
